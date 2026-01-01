1ee6e7983466cc3f6cd4d8978fb8972226c5c886
Theneillsaaco <isaacdepena18@gmail.com>
2026-01-01 19:19:53 -0400

    Refactored `isProductive` and related functions for improved performance and clarity

--- ProductiveNumbers/Productive.fs
+++ ProductiveNumbers/Productive.fs
@@ -3,31 +3,47 @@ namespace ProductiveNumbers
 open ProductiveNumbers.Prime
 
 module Productive =
-    let partition (n:int) =
-        let s = n.ToString()
-        [ for i in 1 .. s.Length - 1 ->
-            let a = int (s.Substring(0,i))
-            let b = int (s.Substring(i))
-            (a, b)
-        ]
         
-    let isProductive n =
-        let baseCheck = isPrime (n + 1)
-        
-        let partitionCheck =
-            partition n
-            |> List.forall (fun (a, b) ->
-                isPrime ((a * b) + 1)
-            )
+    let isProductive (n:int) =
+        if not (isPrime (n + 1)) then
+            false
+        else
+            let s = n.ToString()
+            let len = s.Length
+            let mutable ok = true
+            let mutable i = 1
             
-        baseCheck && partitionCheck
+            while ok && i < len do
+                let a = int (s.Substring(0, i))
+                let b = int (s.Substring(i))
+                
+                if not (isPrime ((a * b) + 1)) then
+                    ok <- false
+                    
+                i <- i + 1
+                
+            ok
     
     let firstNProductives count =
-        Seq.initInfinite (fun i -> i + 10)
-        |> Seq.filter isProductive
-        |> Seq.take count
-        |> Seq.toList
+        let result = ResizeArray<int>()
+        let mutable n = 0
+        
+        while result.Count < count do
+            if isProductive n then
+                result.Add n
+            n <- n + 1
+            
+        result |> List.ofSeq
         
     let nthProductive n =
-        firstNProductives n
-        |> List.last
+        let mutable count = 0
+        let mutable x = 0
+        let mutable result = 0
+        
+        while count < n do
+            if isProductive x then
+                count <- count + 1
+                result <- x
+            x <- x + 1
+            
+        result
